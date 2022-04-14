405420999f84329be857a84f28af5f270a5c36d3
yongbeomkwak <yongbeomkwak@gmail.com>
2022-04-14 23:07:47 +0900

    HW

--- _code/BinaryTree.py
+++ _code/BinaryTree.py
@@ -7,20 +7,6 @@ class BinaryTree:
     def __init__(self,root:Node):
         self.__root:Node=root
     
-    def insert_Node(self,parent:Node,node:Node): #부모노드와 자식노드
-        if(node.getItem()<parent.getItem()): # 부모보다 작으면 왼쪽
-            if(parent.left==None): #비어있으면
-                parent.left=node
-                return
-            else: #있다면 해당 왼쪽을 기준으로 재귀
-                self.insert_Node(parent.left,node)
-        else:
-            if(parent.right==None):#비어있으면
-                parent.right=node
-                return
-            else: #오른쪽으로 재귀
-                self.insert_Node(parent.right,node)
-    
     def getRoot(self)->Node:
         return self.__root
 
@@ -46,5 +32,4 @@ class BinaryTree:
             return
         self.inorder(node.getLeft()) #왼
         self.visit(node) #자기자신
-        self.inorder(node.getRight()) #오
-    
+        self.inorder(node.getRight()) #오    
