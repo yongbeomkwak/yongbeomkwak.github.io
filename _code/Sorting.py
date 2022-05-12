9924210cddd600eb1e766713844b2ec3d84c6539
yongbeomkwak <yongbeomkwak@gmail.com>
2022-05-12 14:40:56 +0900

    Quick

--- _code/Sorting.py
+++ _code/Sorting.py
@@ -1,4 +1,7 @@
 import copy
+from turtle import right
+
+from pandas import pivot
 class Sorting:
 
     def __init__(self,arr:list):
@@ -75,6 +78,69 @@ class Sorting:
                 if(arr[j]>=arr[j-incr]):
                     continue
                 self.swap(arr,j,j-incr) 
+    
+    def merge(self,arr:list,start:int,mid:int,end:int):
+        tmp=[ 0 for _ in  range(end-start+1)]
+        left_idx:int =start #왼쪽 시작지점 
+        right_idx:int =mid+1 #오른쪽 시작지점 
+        k:int=0
+
+        while(left_idx<=mid and right_idx<=end):
+            if(arr[left_idx]<=arr[right_idx]): #왼쪽이 작을 시 
+                tmp[k]=arr[left_idx] #왼쪽 삽입
+                left_idx+=1
+            else:
+                tmp[k]=arr[right_idx] #오른쪽 삽입
+                right_idx+=1
+            k+=1
+
+        # 만약 left_idx 가 mid를 넘었으면 오른쪽 배열이 남은 것
+        entry:int = right if left_idx> mid else left_idx
+        target:int = end if left_idx> mid else mid
+        
+        for i in range(entry,target+1): #나머지 것 옮기고 
+            tmp[k]=arr[i]
+            k+=1
+        
+        p:int=0
+        for i in range(start,end+1): #최종 정렬된 것 본 배열에 옮기기
+            arr[i]=tmp[p]
+            p+=1
+        
+
+
+    def mergeSort(self,arr:list,start:int,end:int):
+        if(start<end):
+            mid=(start+end)//2
+            self.mergeSort(arr,start,mid) #왼쪽
+            self.mergeSort(arr,mid+1,end) #오른쪽
+            self.merge(arr,start,mid,end) #병합
+    
+    def partition(self,arr:list,start:int,end:int):
+        pivot:int=arr[end]
+        i:int =start-1
+        for j in range(start,end):
+            if(arr[j]<=pivot): #pivot보다 작으면 왼쪽 영역을 넓힌 후 스왑
+                i+=1
+                self.swap(arr,i,j)
+        
+        pos:int =i+1 #마지막으로 왼쪽 영역을 넓히고 
+        self.swap(arr,pos,end) #end를 해당 영역에 넣는다(실질적으로 바꾼다)
+        return pos #이후 왼쪽영역 즉, 피벗 영역을 리턴한다
+
+    
+
+
+    def quickSort(self,arr:list,start:int,end:int):
+        if(start<end):
+            q:int=self.partition(arr,start,end) #피벗위치
+            #print("pos: ",start,end,q)
+            self.quickSort(arr,start,q-1) # start~ 피벗 전
+            self.quickSort(arr,q+1,end) # 피벗 후 ~end까지
+
+
+
+
 
 
     def view(self):
--- _code/testAll.py
+++ _code/testAll.py
@@ -4,8 +4,8 @@ if __name__=="__main__":
     a=[i for i in range(30,-1,-1)]
     s=Sorting(a)
     
-    print(s.insertionSort())
-    print(s.bubbleSort())
-    print(s.selectionSort())
-    print(s.shellSort())
-    s.view()
+    s.quickSort(a,0,len(a)-1)
+    print(a)
+
+
+    
