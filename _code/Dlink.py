d10e48e3ab5f3b464a59e1c993e32eb165e7121a
yongbeomkwak <yongbeomkwak@gmail.com>
2022-05-29 00:38:17 +0900

    move DTS

--- _code/Dlink.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from typing import TypeVar
-E = TypeVar('E')
-
-class Dlink():
-    def __init__(self,item:E,prev,next):
-        self.item:E=item
-        self.prev:Dlink=prev
-        self.next:Dlink=next
